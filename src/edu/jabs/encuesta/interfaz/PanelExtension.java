8ae75d958a07d0117c5805d71bff8d809194c4aa
Joan Andrés <andres6936@live.com>
2017-10-24 11:09:12 -0500

    Update UTF-8 PanelExtension.java

--- src/edu/jabs/encuesta/interfaz/PanelExtension.java
+++ src/edu/jabs/encuesta/interfaz/PanelExtension.java
@@ -1,13 +1,14 @@
-/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Universidad de los Andes (Bogot� - Colombia)
- * Departamento de Ingenier�a de Sistemas y Computaci�n 
- * Licenciado bajo el esquema Academic Free License version 2.1 
- *
- * Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
- * Ejercicio: n1_encuesta
- * Autor: Equipo Cupi2 2017
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Universidad Santiago de Cali (Cali - Colombia)
+  Departamento de Ingeniería de Sistemas y Computación
+  Licenciado bajo el esquema Academic Free License version 2.1
+
+  Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+  Ejercicio: N1-Encuesta-Java
+  Autor: Joan Andrés Buriticá Salazar
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
+
 package edu.jabs.encuesta.interfaz;
 
 import java.awt.GridLayout;
@@ -19,7 +20,7 @@ import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
 /**
- * Panel que contiene los botones para ejecutar los puntos de extensi�n.
+ * Panel que contiene los botones para ejecutar los puntos de extensión.
  */
 @SuppressWarnings("serial")
 public class PanelExtension extends JPanel implements ActionListener
@@ -29,12 +30,12 @@ public class PanelExtension extends JPanel implements ActionListener
     // -----------------------------------------------------------------
 
     /**
-     * Comando para el bot�n 1.
+     * Comando para el botón 1.
      */
     private final String OPCION_1 = "OPCION_1";
 
     /**
-     * Comando para el bot�n 2.
+     * Comando para el botón 2.
      */
     private final String OPCION_2 = "OPCION_2";
 
@@ -43,7 +44,7 @@ public class PanelExtension extends JPanel implements ActionListener
     // -----------------------------------------------------------------
 
     /**
-     * Ventana principal de la aplicaci�n.
+     * Ventana principal de la aplicación.
      */
     private InterfazEncuesta principal;
 
@@ -52,12 +53,12 @@ public class PanelExtension extends JPanel implements ActionListener
     // -----------------------------------------------------------------
 
     /**
-     * Bot�n extensi�n 1.
+     * Botón extensión 1.
      */
     private JButton btnOpcion1;
 
     /**
-     * Bot�n extensi�n 2.
+     * Botón extensión 2.
      */
     private JButton btnOpcion2;
 
@@ -66,8 +67,8 @@ public class PanelExtension extends JPanel implements ActionListener
     // -----------------------------------------------------------------
 
     /**
-     * Construye el panel con una referencia a la ventana principal de la aplicaci�n. <br>
-     * <b>post: </b> Construy� el panel.
+     * Construye el panel con una referencia a la ventana principal de la aplicación. <br>
+     * <b>post: </b> Construyó el panel.
      * @param pPrincipal Ventana principal. pPrincipal != null.
      */
     public PanelExtension( InterfazEncuesta pPrincipal )
@@ -77,11 +78,11 @@ public class PanelExtension extends JPanel implements ActionListener
         setBorder( new TitledBorder( "Opciones" ) );
 
         setLayout( new GridLayout( 1, 2 ) );
-        btnOpcion1 = new JButton( "Opci�n 1" );
+        btnOpcion1 = new JButton( "Opción 1" );
         btnOpcion1.setActionCommand( OPCION_1 );
         btnOpcion1.addActionListener( this );
 
-        btnOpcion2 = new JButton( "Opci�n 2" );
+        btnOpcion2 = new JButton( "Opción 2" );
         btnOpcion2.setActionCommand( OPCION_2 );
         btnOpcion2.addActionListener( this );
 
@@ -90,12 +91,12 @@ public class PanelExtension extends JPanel implements ActionListener
     }
 
     // -----------------------------------------------------------------
-    // M�todos
+    // Métodos
     // -----------------------------------------------------------------
 
     /**
      * Manejo de los eventos de los botones.
-     * @param pEvento Evento de click sobre un bot�n. pEvento != null.
+     * @param pEvento Evento de click sobre un botón. pEvento != null.
      */
     public void actionPerformed( ActionEvent pEvento )
     {
